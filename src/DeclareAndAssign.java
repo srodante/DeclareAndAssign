daec8ead11f6ae2b68162a54c63b9975f0f2e2f1
srodante <srodante@gmail.com>
2021-09-22 12:50:58 -0400

    Declare and Assign

---
+++ src/DeclareAndAssign.java
@@ -0,0 +1,16 @@
+public class DeclareAndAssign {
+    public static void main(String[]args){
+        //int i;
+        //i=9;
+        //System.out.println("i");
+        int testScore;
+        float price;
+        String userName;
+        testScore=50;
+        price=24.99f;
+        userName="Bobby Johnson";
+        System.out.println("Test score initialized is: " + testScore);
+        System.out.println("Price tag: " + price);
+        System.out.println("Username: " + userName);
+    }
+}
